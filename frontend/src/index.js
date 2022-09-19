194d236213d60efe331b2235707042f30508ff92
DantasticWebSolutions <74438072+DantasticWebSolutions@users.noreply.github.com>
2022-09-19 22:41:30 +0100

    Favicon

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,26 +7,12 @@ import "./index.css";
 // eslint-disable-next-line
 // import 'swiper/css/bundle';
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+// import * as serviceWorker from "./serviceWorker";
 
-// create a root
 const root = ReactDOM.createRoot(document.getElementById("root"));
-//render app to root
 root.render(
   <Provider store={store}>
     <App />
   </Provider>
 );
-
-// ReactDOM.render(
-//   <Provider store={store}>
-//   <App />
-// </Provider>
-// ,
-//   document.getElementById("root")
-// );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
